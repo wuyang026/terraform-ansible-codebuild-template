ad9ecc554c03d1b50e2653fa6d62cebc4e577ba1
wuyang <yangwu1129@gmail.com>
2026-04-10 10:45:29 +0900

    update

--- ansible/generate_inventory.py
+++ ansible/generate_inventory.py
@@ -11,10 +11,10 @@ with open('inventory/hosts', 'w') as f:
     f.write('[dbservers]\n')
     # プライマリDBサーバー
     for key, instance in outputs['primary_instances']['value'].items():
-        f.write(f'{key} ansible_host={instance["id"]} ansible_connection=aws_ssm ansible_aws_ssm_region=ap-south-1 ansible_aws_ssm_s3_bucket=go-s3-bucket-test ansible_python_interpreter=/usr/bin/python3.9\n')
+        f.write(f'{key} ansible_host={instance["id"]} ansible_connection=aws_ssm ansible_aws_ssm_region=ap-south-1 ansible_python_interpreter=/usr/bin/python3.9\n')
     # スタンバイDBサーバー
     for key, instance in outputs['standby_instances']['value'].items():
-        f.write(f'{key} ansible_host={instance["id"]} ansible_connection=aws_ssm ansible_aws_ssm_region=ap-south-1 ansible_aws_ssm_s3_bucket=go-s3-bucket-test ansible_python_interpreter=/usr/bin/python3.9\n')
+        f.write(f'{key} ansible_host={instance["id"]} ansible_connection=aws_ssm ansible_aws_ssm_region=ap-south-1 ansible_python_interpreter=/usr/bin/python3.9\n')
     f.write('\n[dbservers:vars]\n')
     f.write('ansible_aws_ssm_region=ap-south-1\n')
     f.write('ansible_aws_ssm_s3_bucket=go-s3-bucket-test\n')
